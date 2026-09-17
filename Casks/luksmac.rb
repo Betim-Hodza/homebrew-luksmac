eb1cb89290680624a4f6ecc18c1fa2f542b92528
bay <bhodza02@proton.me>
2026-09-16 21:28:13 -0500

    luksmac 1.0.3

--- Casks/luksmac.rb
+++ Casks/luksmac.rb
@@ -1,6 +1,6 @@
 cask "luksmac" do
-  version "1.0.2"
-  sha256 "4de2ac35ab028139d8702ebfc86386327b9c20f6b20a69c9ce175eab5684ebfc"
+  version "1.0.3"
+  sha256 "612ff6212deec0d6d52a0948e7317e86c975bb5dc7eaa4bbf16d90a6239077a2"
 
   url "https://github.com/Betim-Hodza/luksmac-releases/releases/download/v#{version}/LuksMac-#{version}.pkg"
   name "LuksMac"
